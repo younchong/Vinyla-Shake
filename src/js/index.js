dc973f29c4b1eb4cab0eae3f126a7355535957a2
younchong <ungun96@naver.com>
2022-07-04 11:13:29 +0900

    feat✨ :음악선택 컴포넌트 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
-console.log("Vinyl shake is delicious!!");
+import "../css/index.css";
+import onChange from "./soundWave";
+
+document.querySelector("#left-music-input").addEventListener("change", () => {
+  onChange("left");
+});
+document.querySelector("#right-music-input").addEventListener("change", () => {
+  onChange("right");
+});
