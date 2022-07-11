52752da24b0249e805fc37ae88c3f6613d7c8485
younchong <ungun96@naver.com>
2022-07-11 16:08:42 +0900

    feat/1005 ✨ : Mixing Fader 구현

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,19 @@
 import { Deck } from "./deck";
+import { MixingFaderController } from "./Controller/mixingFaderController";
+import { MixingFaderModel } from "./Model/mixingFaderModel";
+import { MixingFaderView } from "./View/mixingFaderView";
 
 export class App {
   constructor() {
     this.left = new Deck("left");
     this.right = new Deck("right");
+    this.mixingFader = new MixingFaderController(
+      document.querySelector("body"),
+      new MixingFaderModel(null),
+      new MixingFaderView(document.querySelector("body"))
+    );
+
+    this.left.register(this.mixingFader);
+    this.right.register(this.mixingFader);
   }
 }
