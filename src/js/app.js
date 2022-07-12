8ac30b9340c31d0ab9de3b52e42b36e244dc3949
younchong <ungun96@naver.com>
2022-07-12 12:37:51 +0900

    feat/1006 ✨ : 비트메이커 구현

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,9 @@ import { Deck } from "./deck";
 import { MixingFaderController } from "./Controller/mixingFaderController";
 import { MixingFaderModel } from "./Model/mixingFaderModel";
 import { MixingFaderView } from "./View/mixingFaderView";
+import { BeatMakerController } from "./Controller/beatMakerController";
+import { BeatMakerModel } from "./Model/beatMakerModel";
+import { BeatMakerView } from "./View/beatMakerView";
 
 export class App {
   constructor() {
@@ -12,7 +15,11 @@ export class App {
       new MixingFaderModel(null),
       new MixingFaderView(document.querySelector("body"))
     );
-
+    this.beatMaker = new BeatMakerController(
+      document.querySelector("body"),
+      new BeatMakerModel(null),
+      new BeatMakerView(document.querySelector("body"))
+    );
     this.left.register(this.mixingFader);
     this.right.register(this.mixingFader);
   }
