2656b671c9a99c51e6e730929f8146edc35d8ad9
younchong <ungun96@naver.com>
2022-08-01 01:50:30 +0900

    test/4010 ✅ : sound effect click event 스토리북 추가

--- src/stories/EffectComponent.stories.js
+++ src/stories/EffectComponent.stories.js
@@ -1,4 +1,5 @@
 import { createEffectComponent } from "./EffectComponent";
+import { Arrow } from "../js/Utils/Arrow";
 
 export default {
   title: "EffectComponent",
@@ -7,11 +8,15 @@ export default {
   },
 };
 
-const Template = ({onClick}) => {
-  return createEffectComponent({onClick});
+const Template = ({onClick, makeArrow, moveArrow, removeArrow}) => {
+  return createEffectComponent({onClick, makeArrow, moveArrow, removeArrow});
 };
 
 export const Primary = Template.bind({});
+
+let isClicked = false;
+let arrows = [];
+let animation = null;
 
 Primary.args = {
   onClick: () => {
@@ -25,4 +30,66 @@ Primary.args = {
 
     document.querySelector(".deck-effect-title").innerText = nextMode[state];
   },
+  makeArrow: (e) => {
+    isClicked = true;
+
+    const canvas = document.querySelector(".deck-effect-canvas");
+    const x = e.clientX - canvas.offsetLeft;
+    const y = e.clientY - canvas.offsetTop;
+
+    for (let i = 0; i < 15; i++) {
+      arrows.push(
+        new Arrow(canvas.width / 2, canvas.height / 2, { x, y }, document)
+      );
+    }
+
+    playAnimation(0);
+  },
+  moveArrow: (e) => {
+    if (!isClicked) return;
+
+    const canvas = document.querySelector(".deck-effect-canvas");
+    const x = e.clientX - canvas.offsetLeft;
+    const y = e.clientY - canvas.offsetTop;
+
+    arrows.forEach((a) => {
+      a.target = { x, y };
+    });
+  },
+  removeArrow: () => {
+    isClicked = false;
+
+    const canvas = document.querySelector(".deck-effect-canvas");
+    const context = canvas.getContext("2d");
+
+    arrows = [];
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    window.cancelAnimationFrame(animation);
+  }
 };
+
+function playAnimation(frame) {
+  const canvas = document.querySelector(".deck-effect-canvas");
+  const context = canvas.getContext("2d");
+
+  context.fillStyle = "rgba(0, 0, 0, 0.2)";
+  context.globalCompositeOperation = "source-over";
+  context.globalAlpha = 0.2;
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  context.globalAlpha = 1;
+  context.globalCompositeOperation = "lighter";
+  context.lineCap = "round";
+  context.lineJoin = "round";
+
+  arrows.forEach((a) => {
+    a.avoid(arrows);
+  });
+  arrows.forEach((a) => {
+    a.render(frame);
+  });
+
+  frame += 1;
+  animation = requestAnimationFrame(
+    playAnimation.bind(null, frame)
+  );
+}
